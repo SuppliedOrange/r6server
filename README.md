23f7e6d27388ebc1a4cd21b687744fe776dbf37c
SuppliedOrange <blackandantiqual@gmail.com>
2024-01-08 07:41:29 +0530

    Added ubisoft ID selection

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 # r6server
  A server changer tool for Rainbow Six Siege
  <br>
- Just a silly little tool I made to change the r6s server for my friends and I. It speeds up my process. 
+ Just a silly little tool I made to change the r6s server for my friends and I, it speeds up my process. 
 
 ![Screenshot of the application running in a terminal](readme_images/image.png)
 
-# How to use
-Start the .exe and choose the server you want to use.
+<hr>
 <br>
-The app also automatically attempts to start siege (assuming your siege is stored in the C drive in the default steam folder).
+
+# How to install
+Download the .exe from https://github.com/SuppliedOrange/r6server/releases/tag/latest if you want to use the executable. You may also run it with python or build it yourself if you like.
+
+# How to use
+- Start the .exe and choose the server you want to use.
+<br>
+- Select your Ubisoft ID incase you have multiple.
+<br>
+- You may choose to make the app automatically start siege.
+<br>
+- It's recommend you change and customize the constants like your Ubisoft ID and R6S Path to speed up the app's process even more.
 
 # Running as python
-Run `pip install -r requirements.txt` to install all prequisites. Then run `py r6server.py`
+Download the repository. Run `pip install -r requirements.txt` to install all prequisites. Then run `py r6server.py`
 
 # Building
 I used Python 3.10.10. You'll need to get PyInstaller for this. You can change the name and favicon to anything you like.
@@ -22,4 +32,4 @@ PyInstaller Command:
 pyinstaller r6server.py -F -n "R6 Server Changer" -i "favicon.ico"
 ```
 <br>
-You should see it in `/dist`` after it's completed.
+You should see it in `/dist` after it's completed.
--- r6server.py
+++ r6server.py
@@ -1,9 +1,10 @@
 # Imports
+import random
 from consolemenu.format import *
 from modified_console_menu.menu_borders import MenuBorderStyleType as CustomBorderStyleType
 from modified_console_menu.console_menu import *
 from modified_console_menu.function_item import FunctionItem
-from colors import color, strip_color
+from colors import color
 from os import listdir, path, startfile
 import re
 
@@ -44,13 +45,154 @@ SERVER_LIST = {
 }
 
 UUID = "" # Enter your ubisoft connect id if you know it. (located as a folder in C:\Users\(your name)\Documents\My Games\Rainbow Six - Siege)
-
 R6SPATH = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Tom Clancy\'s Rainbow Six Siege\\RainbowSix.exe" # Replace with your R6S path
+START_R6S = False # Asks if you want to start r6s after changing your server.
 
-# A custom Menu object taken from 
+
 # Functions
 
-def generateOptions():
+def switchServer(configPath, server):
+
+    try:
+        with open(configPath, "r") as f: # Open and read the file first
+
+            text = f.readlines()
+            updated_text = text.copy()
+
+            for line in text:
+                if re.match( re.compile(r'DataCenterHint=[a-zA-Z\/\\]+'), line ): # If we find a line that fits the regex of DataCenterHint=(some server)
+                    updated_text[ updated_text.index(line) ] = f"DataCenterHint={server}\n" # We update it and put it in updated_text
+                    break
+
+    except FileNotFoundError: # If the file doesn't exist
+        input(color("Could not find your config file!", fg="red"))
+        exit()
+    
+    with open(configPath, "w") as f: # Open and rewrite the file.
+        f.writelines(updated_text)
+
+    print(color(f"Successfully changed your server to {server}", fg="green"))
+
+    startR6()
+
+    exit(0)
+
+def getUbisoftIds(directory_path):
+
+    ubi_id_dirs = []
+    uuid_pattern = re.compile(r'\b[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\b') # all ubi ids are UUIDs, so we regex search the folders with UUID patterns.
+    all_items = listdir(directory_path)
+
+    # Get all items in the dir
+    for item in all_items:
+        item_path = path.join(directory_path, item)
+
+        # If the item is a directory and it matches the uuid pattern
+        if path.isdir(item_path) and re.match(uuid_pattern, item):
+            ubi_id_dirs.append( re.search(uuid_pattern, item).group(0) )
+
+    return ubi_id_dirs
+
+
+def generateUbisoftIDOptions(settingsDir, server):
+
+    ubiIdOptions = []
+    ubiIds = getUbisoftIds(settingsDir)
+
+    for ubiId in ubiIds:
+
+        # Find each option's index number
+        item_number = ubiIds.index(ubiId) + 1
+
+        # Making a separator with padding
+        separator = "|"
+
+        separator = separator.rjust( 
+            ( len( str( len(ubiIds) ) ) + 1 ) - ( len( str( item_number ) ) ) + 1
+        )
+        
+        separator = separator.ljust( len(separator) + 1 )
+                
+        try: id_name = color(ubiId, applyColor(item_number))
+        except IndexError: id_name = color(ubiId, applyColor(item_number) )
+
+        configPath = settingsDir + rf"\{ubiId}\GameSettings.ini"
+    
+        ubiIdOptions.append(
+            FunctionItem(
+                id_name, switchServer, args=[configPath, server],
+                menu_char=color( item_number, applyColor(item_number) ),
+                index_item_separator= separator
+            )
+        )
+        
+    return ubiIdOptions
+
+def pickUbisoftID(settingsDir, server):
+    
+    menu_format = MenuFormatBuilder()
+    menu_format.set_border_style_type(CustomBorderStyleType.ZERO_BORDER)
+    menu_format.set_subtitle_align('centre')
+
+    menu = ConsoleMenu(NAME_ASCII, f"You have multiple accounts with R6S. Choose the UUID of the account you want.\nThis is located in {settingsDir}", show_exit_option=False, formatter=menu_format)
+
+    for option in generateUbisoftIDOptions(settingsDir, server):
+        menu.append_item(option)
+    
+    menu.show()
+
+def startR6(): 
+        try: # Attempt to start r6s
+
+            if START_R6S:
+                input(color("> Press enter to start Rainbow Six Siege or close this window.\n", fg="blue"))
+                startfile(R6SPATH) # Does not start with vulkan- If you want vulkan, change R6SPATH executable to RainbowSix_Vulkan.exe + You will also need to change the drive folder to wherever you have steam installed.
+            else:
+                input()
+        except: print("Siege failed to start. Is your executable path correct? Ignore this if you didn't want to start siege.")
+
+def applyColor(item_number):
+    try:
+        return list(SERVER_LIST.values())[item_number - 1]
+    except IndexError:
+        return random.choice( list(SERVER_LIST.values()) )
+
+def findConfigFile(server:str):
+
+    settingsDir = path.expanduser('~') + r"\Documents\My Games\Rainbow Six - Siege" # this is where the folders of each account's ubisoft uuid are stored as a directory. inside them, are the config folders
+
+    if not path.exists(settingsDir):
+        print(f"Error: {settingsDir} does not exist")
+        input()
+        exit(1)
+
+    if not UUID: # If there's no preset UUID
+
+        ubi_ids = getUbisoftIds(settingsDir)
+        chosen_ubi_id = None
+
+        if not ubi_ids: # If there were no UUIDs found 
+            print(color(f"Error: There were no ubisoft ID directories found in {settingsDir}", fg="red"))
+            input()
+            exit(1)
+
+        if len(ubi_ids) > 1: # If there's more than 1 ubisoft ID
+            pickUbisoftID(settingsDir, server)
+            return
+        
+        else:
+            chosen_ubi_id = ubi_ids[0]
+        
+        settingsDir += rf"\{chosen_ubi_id}" # pick the first one, even if there are many they are ignored.
+    
+    else:
+        settingsDir += rf"\{UUID}"
+
+    configPath = rf"{settingsDir}\GameSettings.ini"
+
+    switchServer(configPath, server)
+
+def generateServerOptions():
     serverOptions = []
     for server in SERVER_LIST:
 
@@ -72,80 +214,13 @@ def generateOptions():
 
         serverOptions.append(
             FunctionItem(
-                server_name, switchServer, args=[strip_color(server)],
+                server_name, findConfigFile, args=[server],
                 menu_char=color( item_number, fg=SERVER_LIST[server] ),
                 index_item_separator= separator
             )
         )
+
     return serverOptions
-
-def switchServer(server:str):
-
-    settingsDir = path.expanduser('~') + r"\Documents\My Games\Rainbow Six - Siege" # this is where the folders of each account's ubisoft uuid are stored as a directory. inside them, are the config folders
-
-    if not path.exists(settingsDir):
-        print(f"Error: {settingsDir} does not exist")
-        input()
-        exit(1)
-
-    if not UUID:
-        ubi_ids = get_ubisoft_ids(settingsDir)
-
-        if not ubi_ids: 
-            print(color(f"Error: There were no ubisoft ID directories found in {settingsDir}", fg="red"))
-            input()
-            exit(1)
-
-        if len(ubi_ids) > 1:
-            print(color("You have more than 1 ubisoft IDs connected to your R6S documents. The first one found will be used, please delete the folders of the accounts you do not use or modify the code to use your UUID.", fg="red"))
-        
-        settingsDir = ubi_ids[0] # pick the first one, even if there are many they are ignored.
-    
-    else:
-        settingsDir += rf"\{UUID}"
-
-    configPath = rf"{settingsDir}\GameSettings.ini"
-
-    with open(configPath, "r") as f: # Open and read the file first
-
-        text = f.readlines()
-        updated_text = text.copy()
-
-        for line in text:
-            if re.match( re.compile(r'DataCenterHint=[a-zA-Z\/\\]+'), line ): # If we find a line that fits the regex of DataCenterHint=(some server)
-                updated_text[ updated_text.index(line) ] = f"DataCenterHint={server}\n" # We update it and put it in updated_text
-                break
-    
-    with open(configPath, "w") as f: # Open and rewrite the file.
-
-        f.writelines(updated_text)
-
-    print(color(f"Successfully changed your server to {server}", fg="green"))
-    
-    input(color("> Press enter to start Rainbow Six Siege\n", fg="blue"))
-    try: startR6()
-    except: print("Could not start siege! The siege executable path is incorrect.")
-    exit(0)
-
-
-def get_ubisoft_ids(directory_path):
-    ubi_id_dirs = []
-    uuid_pattern = re.compile(r'\b[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\b') # all ubi ids are UUIDs, so we regex search the folders with UUID patterns.
-    all_items = listdir(directory_path)
-
-    # Get all items in the dir
-    for item in all_items:
-        item_path = path.join(directory_path, item)
-
-        # If the item is a directory and it matches the uuid pattern
-        if path.isdir(item_path) and re.match(uuid_pattern, item):
-            ubi_id_dirs.append(item_path)
-
-    return ubi_id_dirs
-    
-
-def startR6(): # Does not start with vulkan- If you want vulkan, change R6SPATH executable to RainbowSix_Vulkan.exe + You will also need to change the drive folder to wherever you have steam installed.
-    startfile(R6SPATH)
 
 def main():
 
@@ -157,7 +232,7 @@ def main():
     menu = ConsoleMenu(NAME_ASCII, "Select a server with it's number", show_exit_option=False, formatter=menu_format)
 
     # Add all the servers to the menu
-    for option in generateOptions():
+    for option in generateServerOptions():
         menu.append_item(option)
 
     # Show the menu
@@ -165,3 +240,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    # For those who wish to navigate this mess of a code voluntarily,
+    # main (gets options from generateServerOptions) -> sends option to findConfigFile -> Sends option either directly to switchServer or pickUbisoftId depending on whether there are many ubisoft IDs or not
+    # pickUbisoftId (gets options from generateUbisoftIdOptions) -> Sends option to switchServer
